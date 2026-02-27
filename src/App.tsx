f35c7a7bff2b39b74543700f642b6ce4488aa3c8
Vincent <vincentmaury20@gmail.com>
2026-02-27 11:34:33 +0100

    feat: Home page and import images

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Home from "./Home/Home";
+
 export default function App() {
   return (
-    <div className="bg-amber-300 text-6xl">
-      <p>Coucou les amis j'y suis enfin arrivé!</p>
+    <div>
+      <Home />
     </div>
   );
 }
