f5a355216cdb18fa193ea8117d523961f9021426
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-29 16:41:39 +0200

    fix: Navigate removed from About.tsx

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,7 +1,6 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import H1Title from "../../ui/H1Title";
 function About() {
-  const navigate = useNavigate();
 
   return (
     <div className="
