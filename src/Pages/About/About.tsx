2c2b701b88f187e33271e638dab355a007997818
Mathieu Bourasseau <mathieubourasseau17@gmail.com>
2026-03-29 16:17:49 +0200

    fix: About presentation and font size fixed

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -4,27 +4,37 @@ function About() {
   const navigate = useNavigate();
 
   return (
-    <div className="max-w-4xl mx-auto p-6 text-center">
+    <div className="
+      max-w-4xl mx-auto p-6 text-center"
+    >
       <H1Title>À PROPOS</H1Title>
-      <p className="text-lg text-white mb-4 text-justify">
-        Bienvenue sur Gamer Challenges, la plateforme ultime pour les passionnés
-        de jeux vidéo ! Nous sommes dédiés à offrir une expérience unique où les
-        joueurs peuvent relever des défis excitants, partager leurs exploits et
-        se connecter avec une communauté dynamique.
-      </p>
-      <p className="text-lg text-white mb-4 text-justify">
-        Chez Gamer Challenges, nous croyons que le jeu est plus qu'un simple
-        passe-temps, c'est une aventure, une compétition et une source de
-        plaisir. Notre mission est de créer un espace où les joueurs peuvent
-        s'amuser, se dépasser et célébrer leurs réussites ensemble.
-      </p>
-      <p className="text-lg text-white mb-4 text-justify">
-        Rejoignez-nous dès aujourd'hui et plongez dans un monde de défis
-        palpitants, de classements compétitifs et de camaraderie entre gamers.
-        Que vous soyez un joueur occasionnel ou un compétiteur chevronné, Gamer
-        Challenges est l'endroit idéal pour vivre votre passion du jeu vidéo à
-        son apogée !
-      </p>
+      <div
+        className="
+          flex flex-col gap-8 text-p-mobile text-justify text-white
+          md:text-p-tablet
+          lg:text-lg lg:gap-12
+        "
+      >
+        <p>
+          Bienvenue sur Gamer Challenges, la plateforme ultime pour les passionnés
+          de jeux vidéo ! Nous sommes dédiés à offrir une expérience unique où les
+          joueurs peuvent relever des défis excitants, partager leurs exploits et
+          se connecter avec une communauté dynamique.
+        </p>
+        <p>
+          Chez Gamer Challenges, nous croyons que le jeu est plus qu'un simple
+          passe-temps, c'est une aventure, une compétition et une source de
+          plaisir. Notre mission est de créer un espace où les joueurs peuvent
+          s'amuser, se dépasser et célébrer leurs réussites ensemble.
+        </p>
+        <p>
+          Rejoignez-nous dès aujourd'hui et plongez dans un monde de défis
+          palpitants, de classements compétitifs et de camaraderie entre gamers.
+          Que vous soyez un joueur occasionnel ou un compétiteur chevronné, Gamer
+          Challenges est l'endroit idéal pour vivre votre passion du jeu vidéo à
+          son apogée !
+        </p>
+      </div>
       <button
         onClick={() => navigate("/")}
         className="mt-6 px-6 py-3 uppercase font-bold bg-green-light text-whitefont-semibold rounded-4xl hover:bg-white hover:text-green-light border-2 border-green-light transition-colors">
